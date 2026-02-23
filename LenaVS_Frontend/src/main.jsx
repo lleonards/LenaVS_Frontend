2f6b105ef779cd1edd66febec86e882857d07f31
lleonards <lleonards.777@gmail.com>
2026-02-23 11:36:42 -0300

    Update main.jsx

--- LenaVS_Frontend/src/main.jsx
+++ LenaVS_Frontend/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App' // Removi o .jsx para evitar problemas de resolução de nomes em alguns builds
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// 🔎 LOG DE DIAGNÓSTICO: Se este log NÃO aparecer no console (F12), 
+// o problema é o arquivo index.html que não está conseguindo carregar o main.jsx.
+console.log('Main.jsx carregado com sucesso!');
+
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  console.error('ERRO CRÍTICO: Elemento root não encontrado no index.html');
+} else {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
